9f6974291067c0d546d80772f54d996181d48480
MarkMoukarzel <mark_moukarzel@hotmail.com>
2018-01-22 10:40:18 +0200

    First Commit
    Test First Comit

---
+++ TechDemo/Intermediate/Build/Win64/UE4Editor/Inc/TechDemo/TechDemoGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TECHDEMO_TechDemoGameModeBase_generated_h
+#error "TechDemoGameModeBase.generated.h already included, missing '#pragma once' in TechDemoGameModeBase.h"
+#endif
+#define TECHDEMO_TechDemoGameModeBase_generated_h
+
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_RPC_WRAPPERS
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATechDemoGameModeBase(); \
+	friend TECHDEMO_API class UClass* Z_Construct_UClass_ATechDemoGameModeBase(); \
+public: \
+	DECLARE_CLASS(ATechDemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/TechDemo"), NO_API) \
+	DECLARE_SERIALIZER(ATechDemoGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATechDemoGameModeBase(); \
+	friend TECHDEMO_API class UClass* Z_Construct_UClass_ATechDemoGameModeBase(); \
+public: \
+	DECLARE_CLASS(ATechDemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/TechDemo"), NO_API) \
+	DECLARE_SERIALIZER(ATechDemoGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATechDemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATechDemoGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATechDemoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATechDemoGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATechDemoGameModeBase(ATechDemoGameModeBase&&); \
+	NO_API ATechDemoGameModeBase(const ATechDemoGameModeBase&); \
+public:
+
+
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATechDemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATechDemoGameModeBase(ATechDemoGameModeBase&&); \
+	NO_API ATechDemoGameModeBase(const ATechDemoGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATechDemoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATechDemoGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATechDemoGameModeBase)
+
+
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_12_PROLOG
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_RPC_WRAPPERS \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_INCLASS \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TechDemo_Source_TechDemo_TechDemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TechDemo_Source_TechDemo_TechDemoGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
